3813568e7bc53cca1a6ee84a7449cd8215eaaad5
Ailton-exe <ailton.ramos23b@udgvirtual.udg.mx>
2024-04-13 16:14:56 -0600

    Primera versión del proyecto
    Se incluye la landing home del proyecto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaSquareYoutube } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+
+const Footer = () => {
+  const loginState = useSelector((state) => state.auth.isLoggedIn);
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-10 max-md:px-0">
+      <nav className="grid grid-flow-col max-sm:grid-flow-row gap-4">
+        <Link to="/" className="link link-hover text-2xl max-md:text-xl text-accent-content" onClick={() => window.scrollTo(0, 0)}>
+          Inicio
+        </Link>
+        <Link to="/" className="link link-hover text-2xl max-md:text-xl text-accent-content" onClick={() => window.scrollTo(0, 0)}>
+          Tienda
+        </Link>
+        <Link to="/" className="link link-hover text-2xl max-md:text-xl text-accent-content" onClick={() => window.scrollTo(0, 0)}>
+          Sobre nosotros
+        </Link>
+        <Link to="/" className="link link-hover text-2xl max-md:text-xl text-accent-content" onClick={() => window.scrollTo(0, 0)}>
+          Contacto
+        </Link>
+        {!loginState && (
+          <>
+            <Link
+              to="/"
+              className="link link-hover text-2xl max-md:text-xl text-accent-content"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              Iniciar sesión
+            </Link>
+            <Link
+              to="/"
+              className="link link-hover text-2xl max-md:text-xl text-accent-content"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              Registrarse
+            </Link>
+          </>
+        )}
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <FaSquareXTwitter className="text-6xl max-sm:text-4xl text-accent-content" />
+          <FaSquareFacebook className="text-6xl max-sm:text-4xl text-accent-content" />
+          <FaSquareInstagram className="text-6xl max-sm:text-4xl text-accent-content" />
+          <FaSquareYoutube className="text-6xl max-sm:text-4xl text-accent-content" />
+        </div>
+      </nav>
+      <aside>
+        <p className="text-2xl max-sm:text-sm text-accent-content">
+          Copyright © 2023 - Todos los derechos reservados
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
